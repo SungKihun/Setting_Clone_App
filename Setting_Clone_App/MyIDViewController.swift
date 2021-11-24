2da97ec6c7726b2809c6d915d9da39e35f25b8b6
SungKihun <tjdrlgns3293@gmail.com>
2021-11-24 20:13:59 +0900

    Apple 로그인 UI 구성

---
+++ Setting_Clone_App/MyIDViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MyIDViewController.swift
+//  Setting_Clone_App
+//
+//  Created by 성기훈 on 2021/11/24.
+//
+
+import UIKit
+
+class MyIDViewController: UIViewController {
+
+    @IBOutlet var nextButton: UIButton!
+    @IBOutlet var cancelButton: UIButton!
+    
+    @IBOutlet var emailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
